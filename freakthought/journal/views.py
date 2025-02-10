98d4500bdd9b3d04b32fe274f9bd2cf5c86b9bb4
freakpari <parivashcheraghi40@gmail.com>
2025-02-10 17:25:57 +0330

    Integrating flash messages

--- freakthought/journal/views.py
+++ freakthought/journal/views.py
@@ -14,6 +14,7 @@ def register(request):
         form = CreateUserForm(request.POST)  
         if form.is_valid():  
             form.save()
+            messages.success(request,"user created successfully!")
             return redirect('my-login')  
 
     context = {'RegisterationForm': form}
