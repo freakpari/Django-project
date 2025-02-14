079917fb0647c0ee8e84009a5f9a2c86f2ce3990
freakpari <parivashcheraghi40@gmail.com>
2025-02-14 15:20:16 +0330

    allowed files upload

--- freakthought/freakthought/settings.py
+++ freakthought/freakthought/settings.py
@@ -110,6 +110,10 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.1/howto/static-files/
 
+MEDIA_URL = 'media/'
+MEDIA_ROOT = BASE_DIR /'media'
+
+
 STATIC_URL = 'static/'
 STATICFILES_DIRS = [BASE_DIR / 'static']
 
